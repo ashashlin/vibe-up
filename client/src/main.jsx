00b872afeb1387231b9c876ca7a65216799b1a4b
Ashley Lin <linashleyy@gmail.com>
2025-06-25 22:49:33 +0800

    Fix: import typo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { EventsProvider } from "./contexts/eventsContext.jsx";
+import { EventsProvider } from "./contexts/EventsContext.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
