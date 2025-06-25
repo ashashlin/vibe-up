80b0e1b39681322647154d8a85ec993d3e697cf1
Ashley Lin <linashleyy@gmail.com>
2025-06-25 20:45:20 +0800

    Fix: enable SSL for Render DB

--- server/db/client.js
+++ server/db/client.js
@@ -1,5 +1,10 @@
 import pg from "pg";
 
-const db = new pg.Client(process.env.DATABASE_URL);
+const db = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default db;
